71858347704e1d15a779106e53832d5247a5124f
Marcel Ramos <marceldavid@hotmail.com>
2025-05-16 15:12:51 -0400

    movies details set

--- src/App.js
+++ src/App.js
@@ -1,51 +1,5 @@
 import { useEffect, useState } from "react";
-
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
-
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
+import StarRating from "./StarRating";
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -61,14 +15,25 @@ export default function App() {
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
+  const [query, setQuery] = useState("");
+  const [selectedId, setSelectedId] = useState(null);
 
-  const query = "Star Wars";
+  function handleSelectedMovie(id) {
+    // id will be get when user click on the movies <li>
+    setSelectedId(selectedId === id ? null : id);
+  }
+
+  function handleCloseSelected() {
+    setMovies([]);
+  }
 
   useEffect(
     function () {
       async function fetchMovies() {
         try {
           setIsLoading(true);
+          setError("");
+
           const res = await fetch(
             `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
           );
@@ -83,21 +48,25 @@ export default function App() {
 
           setMovies(data.Search);
         } catch (err) {
-          console.log(err.message);
           setError(err.message);
         } finally {
           setIsLoading(false);
         }
       } // fetchMovies
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
       fetchMovies();
     }, //outter function,
-    []
+    [query]
   ); //useEffect
 
   return (
     <>
       <NavBar>
-        <SearchBar />
+        <SearchBar queryProp={query} setQueryProp={setQuery} />
         <NumResults moviesProp={movies} />
       </NavBar>
 
@@ -105,14 +74,26 @@ export default function App() {
         <Box>
           {/* {isLoading ? <Load /> : <MovieList moviesProp={movies} />} */}
           {isLoading && <Loader />}
-          {!isLoading && !error && <MovieList moviesProp={movies} />}
+          {!isLoading && !error && (
+            <MovieList
+              moviesProp={movies}
+              onSelectedMovieP={handleSelectedMovie}
+            />
+          )}
           {error && <ErrorMessage messageProp={error} />}
         </Box>
         <Box>
-          <>
-            <WatchedSumary watchedProp={watched} />
-            <WatchedMovieList watchedProp={watched} />
-          </>
+          {selectedId ? (
+            <MovieDetails
+              selectedIdProp={selectedId}
+              onCloseP={handleCloseSelected}
+            />
+          ) : (
+            <>
+              <WatchedSumary watchedProp={watched} />
+              <WatchedMovieList watchedProp={watched} />
+            </>
+          )}
         </Box>
       </Main>
     </>
@@ -141,15 +122,14 @@ function NavBar({ children }) {
   );
 }
 //////////////////////////////////////////////////////////////////////
-function SearchBar() {
-  const [query, setQuery] = useState("");
+function SearchBar({ queryProp, setQueryProp }) {
   return (
     <input
       className="search"
       type="text"
       placeholder="Search movies..."
-      value={query}
-      onChange={(e) => setQuery(e.target.value)}
+      value={queryProp}
+      onChange={(e) => setQueryProp(e.target.value)}
     />
   );
 }
@@ -196,26 +176,101 @@ function Box({ children }) {
   );
 }
 ///////////////////////////////////////////////////////////////////////
-function MovieList({ moviesProp }) {
+function MovieList({ moviesProp, handleClick, onSelectedMovieP }) {
   return (
-    <ul className="list">
+    <ul className="list list-movies">
       {moviesProp.map((movie) => (
-        <Movie movieMapProp={movie} key={movie.imdbID} />
+        <Movie
+          movieMapProp={movie}
+          key={movie.imdbID}
+          handleClickProp={handleClick}
+          onSelectedMovieP={onSelectedMovieP}
+        />
       ))}
     </ul>
   );
 }
 //////////////////////////////////////////////////////////////////////
-function Movie({ movieMapProp }) {
+function Movie({ movieMapProp, onSelectedMovieP }) {
   return (
-    <li>
+    <li onClick={() => onSelectedMovieP(movieMapProp.imdbID)}>
       <img src={movieMapProp.Poster} alt={`${movieMapProp.Title} poster`} />
       <h3>{movieMapProp.Title}</h3>
       <p>📅 {movieMapProp.Year}</p>
     </li>
   );
 }
+//////////////////////////////////////////////////////////////////////
+function MovieDetails({ selectedIdProp, onCloseP }) {
+  const [movie, setMovie] = useState({});
 
+  //destructuring data out of the movie obj and giving properties a lowercase name:
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  //Each time this component mounts, The movie correspondind to the selectedID
+  //we be fetched.
+  useEffect(
+    function () {
+      async function getMoviesDetails() {
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedIdProp}`
+        );
+        const data = await res.json();
+
+        setMovie(data);
+      }
+      getMoviesDetails();
+    },
+    [selectedIdProp]
+  );
+
+  return (
+    <div className="details">
+      <header>
+        <button className="btn-black" onClick={onCloseP}>
+          &larr;
+        </button>
+
+        <img src={poster} alt={`Posster of ${movie} movie`} />
+        <div className="details-overview">
+          <h2>{title}</h2>
+          <p>
+            {released} &bull; {runtime}
+          </p>
+          <p>{genre}</p>
+          <p>
+            <span>⭐️</span>
+            {imdbRating} IMDb rating
+          </p>
+        </div>
+      </header>
+
+      <div className="rating">
+        <StarRating maxRating={10} size={30} />
+      </div>
+
+      <section>
+        <p>
+          <em>{plot}</em>
+        </p>
+        <p>Starring {actors}</p>
+        <p>Directed by {director}</p>
+      </section>
+    </div>
+  );
+}
+//////////////////////////////////////////////////////////////////////
 function WatchedSumary({ watchedProp }) {
   // watchedProp.map((movie) => movie.imdbRating) cria um array só com
   // as notas imdbRating quem em seguida é passado p/ a função average p/
